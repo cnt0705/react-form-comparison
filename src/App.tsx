22252d2fb60ea55ddc0e582923b06de4f5404863
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-02-28 17:50:23 +0900

    Create form view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import CssBaseline from '@material-ui/core/CssBaseline'
+import { CssBaseline } from '@material-ui/core'
 
 import { FormikContainer } from 'containers/formik'
 
--- src/components/formik/index.tsx
+++ src/components/formik/index.tsx
@@ -1,3 +1,45 @@
 import React from 'react'
+import { Button, Container, Grid, MenuItem, TextField } from '@material-ui/core'
 
-export const FormikPage: React.FC = () => <div>Formik</div>
+const options = [
+  {
+    value: 1,
+    label: 'Watching K-drama',
+  },
+  {
+    value: 2,
+    label: 'Hanging out with my friend',
+  },
+  {
+    value: 3,
+    label: 'Staying at home',
+  },
+]
+
+export const FormikPage: React.FC = () => {
+  return (
+    <Container maxWidth="sm">
+      <form action="">
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <TextField id="name" label="Name" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id="favorite" label="What You Like" fullWidth select>
+              {options.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  )
+}
